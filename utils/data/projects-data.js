7c87f76d41c4cae0eb046c17fc4b9a27f5810152
John <appy1828@gmail.com>
2025-12-16 15:27:54 +0800

    Revise projectsData with new project details
    Updated project details for the Data Analysis & Visualization App and added a new project entry.

--- utils/data/projects-data.js
+++ utils/data/projects-data.js
@@ -19,6 +19,15 @@ export const projectsData = [
     },
     {
         id: 3,
+        name: 'Data Analysis & Visualization App',
+        description: 'Designed and developed a full-stack web app for data analysis and visualizations, leveraging on OenAi Responses API. I created the UI using NextJS, Typescript, TailwindCSS, and react. The app supports multiple models from Anthropic. developed the API using NestJS, Typescript, PostgresSQL. Deployed the back-end app to GCP.',
+        tools: ["Claude API",'OpenAI', 'NextJS', 'Tailwind CSS', "TypeScript", "GCP"],
+        role: 'Frontend & Backend Developer',
+        code: '',
+        demo: 'https://app-visual-1042163124761.asia-southeast1.run.app/',
+    },
+    {
+        id: 4,
         name: 'AI Powered Financial App',
         description: "My team built an AI-powered financial mobile application. I have developed API using Express, Typescript, OpenAI, AWS, and MongoDB. Used OTP via AWS SES, Google, and Facebook for the authentication system. Built AI assistants using OpenAI's latest model and trained using our dataset. Voice messages are converted to text using AWS Transcribe. The app fetches data from Google Sheets and generates a PDF term sheet, sent via AWS SES.",
         tools: ['Express', 'MongoDB', 'OpenAI API', 'AWS SES', 'AWS S3', 'Node Mailer', 'Joi', 'Puppeteer', 'EC2', 'PM2', 'Nginx'],
@@ -40,4 +49,5 @@ export const projectsData = [
 //     role: '',
 //     code: '',
 //     demo: '',
+
 // },
